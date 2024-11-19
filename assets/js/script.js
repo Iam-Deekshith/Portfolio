668e388d6d7771b6044afa71d4846697ae1f791c
dxithB <dxith23@gmail.com>
2024-11-19 14:19:16 +0530

    names changed

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,7 +59,7 @@ $(document).ready(function () {
 document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
-            document.title = "Portfolio | Chaitanya";
+            document.title = "Portfolio | Deekshith";
             $("#favicon").attr("href", "assets/images/favicon.png");
         }
         else {
